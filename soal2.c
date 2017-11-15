c6edc70645a901ea8845f66c15f5268ff6318c1b
abyan28 <v28mtyqm.mu1@gmail.com>
2017-11-15 17:27:49 +0700

    2

--- soal2.c
+++ soal2.c
@@ -11,4 +11,21 @@
 //alamat file system yang akan di mount
 static const char *dirpath = "/home/v28cyber/Documents";
 
+/*fungsi untuk mengambil atribut. fungsi ini akan dipanggil
+setiap fungsi xmp readdir membaca isi direktori*/
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+
+	int res;
+	char fpath[1000];
+
+  	sprintf(fpath,"%s%s",dirpath, path);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
 
