de7e6a92d6e303af4144cdef23cacbe5a8401c7e
abyan28 <v28mtyqm.mu1@gmail.com>
2017-11-15 17:26:24 +0700

    1

---
+++ soal2.c
@@ -0,0 +1,14 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+//alamat file system yang akan di mount
+static const char *dirpath = "/home/v28cyber/Documents";
+
+
