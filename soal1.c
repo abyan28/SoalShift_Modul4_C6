3fe9c044db3127ff85e390444a15c2a0fc178f2c
abyan28 <v28mtyqm.mu1@gmail.com>
2017-11-15 13:03:23 +0700

    2

--- soal1.c
+++ soal1.c
@@ -7,3 +7,37 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+
+
+static const char *dirpath = "/home/v28cyber/Documents";
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+
+{
+
+	int res;
+	char fpath[1000];
+	char newFile[100];
+
+  	printf("path   : %s, len: %d\n", path, strlen(path));
+
+	if (strcmp(path, "/") != 0) {
+		memcpy(newFile, path, strlen(path) - 4);
+		newFile[strlen(path) - 4] = '\0';
+	}
+	else 
+	{
+		memcpy(newFile, path, strlen(path));
+  	}
+
+	printf("newFile: %s\n", newFile);
+	sprintf(fpath,"%s%s",dirpath, newFile);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+
+}
+
