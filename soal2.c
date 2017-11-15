2106c20ce313ff5196100d879e99e9e991a565ba
abyan28 <v28mtyqm.mu1@gmail.com>
2017-11-15 17:30:04 +0700

    4

--- soal2.c
+++ soal2.c
@@ -61,4 +61,52 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
 	return 0;
 }
 
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+	char fpath[1000];
+	 //sama seperti di get atrribute, cuma ini buat bisa untuk di read berdasarkan nama aslinya
+
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else {
+		sprintf(fpath, "%s%s",dirpath,path);
+	}
+
+	int res = 0;
+	int fd = 0;
+	(void) fi;
+	
+     	if(strstr(fpath,".doc") != 0 || strstr(fpath, ".pdf") != 0 || strstr(fpath, ".txt") != 0)
+	{
+		char file[100];
+		char ext[50];
+		int rm;		
+		system("zenity --error --text='Terjadi kesalahan! File berisi konten berbahaya.'");		
+		sprintf(file,"%s",fpath);
+		sprintf(ext,"%s.ditandai",file);
+		
+		rm = rename(file, ext);
+		if(rm == -1) 
+			return -errno;
+		return -1;
+      	}
+  	else{
+		fd = open(fpath, O_RDONLY);
+
+		if (fd == -1)
+			return -errno;
+
+		res = pread(fd, buf, size, offset);
+
+		if (res == -1)
+			res = -errno;
+
+		close(fd);
+	}
+	return res;
+}
+
 
